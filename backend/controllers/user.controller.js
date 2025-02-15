51a7803a8a2c2257d5c726c2d533f7efca224e23
Dev Bhattacharya <devbhattacharya42@gmail.com>
2025-02-15 17:53:00 +0530

    register route created using the mvc architecture.
    register route created using the mvc architecture.

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,27 @@
+const { validationResult } = require("express-validator");
+const userModel = require("../models/User.model");
+const userService = require("../services/user.service");
+
+module.exports.registerUser = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(400).json({ errors: errors.array() });
+  }
+  const { fullName, email, password } = req.body;
+
+  console.log(req.body);
+
+  const hashedPassword = await userModel.hashPassword(password);
+
+  const user = await userService.createUser({
+    firstName: fullName.firstName,
+    lastName: fullName.lastName,
+    email,
+    password: hashedPassword,
+  });
+
+  const token = user.generateAuthToken();
+  res
+    .status(201)
+    .json({ message: "User created successfully", user: user, token: token });
+};
