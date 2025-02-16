dbca0d6e5d04e03fb9d9ea659b556abdcb150033
Dev Bhattacharya <devbhattacharya42@gmail.com>
2025-02-16 20:31:14 +0530

    routes for /users/profile and /users/logout functionality added
    routes for /users/profile and /users/logout functionality added

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const connectToDB = require("../backend/db/db");
 const userRoutes = require("../backend/routes/user.routes");
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 connectToDB();
 
