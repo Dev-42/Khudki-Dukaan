602ba65b866d019383b83be4eea5012b42ad2a58
Dev Bhattacharya <devbhattacharya42@gmail.com>
2025-02-18 15:24:57 +0530

    Initiated the homepage
    Initiated the homepage

---
+++ frontend/khudki-dukaan/src/components/pages/Homepage.js
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1>I am saying hello from the homepage</h1>
+    </div>
+  );
+};
+
+export default Homepage;
